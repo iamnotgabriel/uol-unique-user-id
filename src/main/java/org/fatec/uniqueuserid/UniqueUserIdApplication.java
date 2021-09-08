2b88eaea35826d6dd38a6866b249345869f3d1d4
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-09-08 07:50:33 -0300

    add: spring init

---
+++ src/main/java/org/fatec/uniqueuserid/UniqueUserIdApplication.java
@@ -0,0 +1,13 @@
+package org.fatec.uniqueuserid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueUserIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueUserIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fatec/uniqueuserid/UniqueUserIdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fatec.uniqueuserid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueUserIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
