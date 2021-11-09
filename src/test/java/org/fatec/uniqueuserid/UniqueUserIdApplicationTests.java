7af1c7a83e98d673ca27ad5243373e731da82b21
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-11-09 00:02:17 -0300

    fix: imports

--- src/test/java/org/fatec/uniqueuserid/UniqueUserIdApplicationTests.java
+++ src/test/java/org/fatec/uniqueuserid/UniqueUserIdApplicationTests.java
@@ -1,5 +1,8 @@
 package org.fatec.uniqueuserid;
 
+import javax.transaction.Transactional;
+import org.springframework.test.annotation.Rollback;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
